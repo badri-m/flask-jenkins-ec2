aadd00aef70b36d7468b277ecf8ba6630993853b
Badri Narayanan <165938110+badri-m@users.noreply.github.com>
2025-12-19 21:13:16 +0530

    testing trigger

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return " 🏁 well done the flask application is running successfully using jenkins + docker "
+    return " 🏁 well done the flask application is running successfully using jenkins + docker via ec2 "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
